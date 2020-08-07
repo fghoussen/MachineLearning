69078c9a3fc6668ac072816a37446f4de675e45b
Franck HOUSSEN <fghoussen@users.noreply.github.com>
2020-08-07 17:35:29 +0200

    poly example: regression (bootstraping / boosting)

---
+++ 1.supervised/1.regression/3.ensemble/2.sequential/poly.py
@@ -0,0 +1,60 @@
+#!/usr/bin/python
+# -*- coding: UTF-8 -*-
+
+import numpy as np
+import matplotlib.pyplot as plt
+from sklearn.ensemble import AdaBoostRegressor, GradientBoostingRegressor
+from sklearn.neighbors import KNeighborsRegressor
+from sklearn.svm import SVR
+from sklearn.kernel_ridge import KernelRidge
+
+def f(x):
+    return x*np.cos(x) + np.random.normal(size=500)*2
+
+def main():
+    # Generate random data : create random points, and, keep only a subset of them.
+    x = np.linspace(0, 10, 500)
+    rng = np.random.RandomState(0)
+    rng.shuffle(x)
+    x = np.sort(x[:])
+    y = f(x)
+
+    # Create bagging and random forest models.
+    fig, axes = plt.subplots(2, 2, figsize=(20, 10))
+    regressors = [AdaBoostRegressor(n_estimators=5, base_estimator=KNeighborsRegressor()),
+                  AdaBoostRegressor(n_estimators=5, base_estimator=SVR()),
+                  AdaBoostRegressor(n_estimators=5, base_estimator=KernelRidge()),
+                  GradientBoostingRegressor()]
+    for ax, reg in zip(axes.ravel(), regressors):
+        # Set title.
+        title = reg.__class__.__name__
+        reg_params = reg.get_params()
+        if 'base_estimator' in reg_params: # GradientBoostingRegressor has no 'base_estimator'.
+            title += ', estimator: '+reg_params['base_estimator'].__class__.__name__
+        ax.set_title(title)
+
+        # Plot random data.
+        ax.plot(x, y, 'o', color='black', markersize=2, label='random data')
+
+        # Create augmented data : add dimensions to initial data in order to fit y as a polynomial of degree 5.
+        x_augmented = np.array([x, x**2, x**3, x**4, x**5]).T
+
+        # Train model.
+        reg.fit(x_augmented, y)
+
+        # Plot intermediate regression estimations.
+        if isinstance(reg, AdaBoostRegressor):
+            for i, tree in enumerate(reg.estimators_):
+                ax.plot(x_augmented[:, 0], tree.predict(x_augmented), '-', color='gray', label='tree '+str(i))
+                ax.axis('off')
+                ax.legend()
+
+        # Plot final regression.
+        ax.plot(x_augmented[:, 0], reg.predict(x_augmented), '-', color='red', label='bagging')
+        ax.axis('off')
+        ax.legend()
+    plt.show()
+
+if __name__ == '__main__':
+    # https://openclassrooms.com/fr/courses/4470521-modelisez-vos-donnees-avec-les-methodes-ensemblistes/4664692-initiez-vous-aux-methodes-sequentielles-et-au-boosting
+    main()
