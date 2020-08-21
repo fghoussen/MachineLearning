8ca08a765abd11c469fb2282ec39b2f187b25903
Franck HOUSSEN <fghoussen@users.noreply.github.com>
2020-08-21 18:56:17 +0200

    poly example: add k-NN.

--- 1.supervised/1.regression/2.nonlinear/poly.py
+++ 1.supervised/1.regression/2.nonlinear/poly.py
@@ -3,7 +3,7 @@
 
 import numpy as np
 import matplotlib.pyplot as plt
-from sklearn import linear_model
+from sklearn import linear_model, neighbors
 
 def f(x):
     return x*np.cos(x) + np.random.normal(size=500)*2
@@ -22,12 +22,17 @@ def main():
     # Create augmented data : add dimensions to initial data in order to fit y as a polynomial of degree 5.
     x_augmented = np.array([x, x**2, x**3, x**4, x**5]).T
 
-    # Polynomial regression : linear regression on augmented data.
-    regr = linear_model.LinearRegression()
-    regr.fit(x_augmented, y)
+    # Polynomial regression : regression on augmented data.
+    regrs = []
+    regrs.append((linear_model.LinearRegression(), 'polynomial reg'))
+    regrs.append((neighbors.KNeighborsRegressor(15), '15-NN reg'))
+    for regr in regrs:
+        model, lbl = regr[0], regr[1]
 
-    # Plot polynomial regression.
-    plt.plot(x_augmented[:,0], regr.predict(x_augmented), '-', color='orange', label='polynomial regression')
+        # Train model.
+        model.fit(x_augmented, y)
+        # Plot regression.
+        plt.plot(x_augmented[:,0], model.predict(x_augmented), '-', label=lbl)
     plt.axis('off')
     plt.legend()
     plt.show()
