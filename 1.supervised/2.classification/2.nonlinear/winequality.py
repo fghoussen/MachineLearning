741154b39304dab4d99d6d45cf1f3f994587aeda
Franck HOUSSEN <fghoussen@users.noreply.github.com>
2020-08-23 19:48:14 +0200

    wine example: show variable magnitude before / after scaling.

--- 1.supervised/2.classification/2.nonlinear/winequality.py
+++ 1.supervised/2.classification/2.nonlinear/winequality.py
@@ -24,8 +24,23 @@ def main():
 
     # Scale data to reduce weights.
     # https://openclassrooms.com/fr/courses/4444646-entrainez-un-modele-predictif-lineaire/4507801-reduisez-l-amplitude-des-poids-affectes-a-vos-variables
+    # https://openclassrooms.com/fr/courses/4297211-evaluez-les-performances-dun-modele-de-machine-learning/4308246-tp-selectionnez-le-nombre-de-voisins-dans-un-knn
     std_scale = preprocessing.StandardScaler().fit(x)
     x_scaled = std_scale.transform(x)
+
+    for var, lbl in zip([x, x_scaled], ['not scaled', 'scaled']):
+        fig, all_axis = plt.subplots(3, 4)
+        for feat_idx in range(var.shape[1]):
+            # variable alone.
+            axis = all_axis.ravel()[feat_idx]
+            axis.hist(var[:, feat_idx], bins=50)
+            axis.set_title(raw_data.columns[feat_idx]+' - '+lbl, fontsize=14)
+            # variable superimposed with others.
+            last_axis = all_axis.ravel()[11]
+            last_axis.hist(var[:, feat_idx], bins=50)
+            last_axis.set_title('whole dataset - '+lbl, fontsize=14)
+        plt.subplots_adjust(left=0.1, bottom=0.1, right=0.9, top=0.9, wspace=0.3, hspace=0.3)
+        plt.show() # Show variable magnitude before / after scaling.
 
     # Split data set into training set and testing set.
     # https://openclassrooms.com/fr/courses/4011851-initiez-vous-au-machine-learning/4020631-exploitez-votre-jeu-de-donnees
